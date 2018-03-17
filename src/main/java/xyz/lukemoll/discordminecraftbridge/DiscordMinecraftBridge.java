64524a1ba0678fce4d0197b6b1faac41234e65d9
Luke Moll <luke.moll99@gmail.com>
2018-03-17 15:03:10 +0000

    Added onEnable and onDisable, player login and chat listeners, all with basic bodies. @ 0.0.2

--- src/main/java/xyz/lukemoll/discordminecraftbridge/DiscordMinecraftBridge.java
+++ src/main/java/xyz/lukemoll/discordminecraftbridge/DiscordMinecraftBridge.java
@@ -1,7 +1,40 @@
 package xyz.lukemoll.discordminecraftbridge;
 
+import java.util.logging.Logger;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class DiscordMinecraftBridge extends JavaPlugin {
+public class DiscordMinecraftBridge extends JavaPlugin implements Listener {
 
+	private Logger log = this.getLogger();
+	
+	@Override
+	public void onEnable() {
+		log.info("onEnable()");
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+	
+	@Override
+	public void onDisable() {
+		log.info("onDisable();");
+		// Do we need to deregister events?
+	}
+	
+	
+	/******************** Event handlers **********************/
+	
+	@EventHandler
+	public void onLogin(PlayerLoginEvent event) {
+		log.info("--> " + event.getPlayer().getDisplayName());
+	}
+	
+	@EventHandler
+	public void onChat(AsyncPlayerChatEvent event) {
+		log.info(event.getPlayer().getDisplayName()+": "+event.getMessage());
+	}
+	
 }
